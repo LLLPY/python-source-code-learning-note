38f2591f53580cb09088c2f18d5d0b0f1034ae38
lll <2474605919@qq.com>
2024-04-14 13:31:26 +0800

    perf:优化编译脚本

--- tools/build.sh
+++ tools/build.sh
@@ -4,17 +4,16 @@ SCRIPT=$(readlink -f "$0")
 # 获取当前脚本文件所在的目录
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
-echo "当前脚本文件所在的目录是：$SCRIPT_DIR"
-
 # 切换到根目录
-project_dir="$SCRIPT_DIR/.."
+project_dir=$(dirname "$SCRIPT_DIR")
+echo "当前的项目目录是：$SCRIPT_DIR"
 
 # 删除旧的解释器
-rm -rf dist
-rm -rf venv
+rm -rf $project_dir/dist/*
+rm -rf $project_dir/venv
 
 # 执行编译命令
-cd "$project_dir/Python-3.11.7" && ./configure  --prefix="$project_dir/dist" && make -j install
+cd $project_dir/Python-3.11.7 && ./configure  --prefix=$project_dir/dist && make -j install
 
 # 创建新的虚拟环境
 cd $project_dir && ./dist/bin/python3 -m venv venv && source venv/bin/activate 
